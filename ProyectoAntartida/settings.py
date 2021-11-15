c1b66b006ad73dd4b3058e8de464b6719d3dbf09
Cristian <cristianariel69@gmail.com>
2021-11-14 21:04:05 -0300

    agregadas variables de entorno

--- ProyectoAntartida/settings.py
+++ ProyectoAntartida/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ot+rhv%g7i56jovwk-n#9r8e7=(u+ec%lleq+xtj3qah!$dzi'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
